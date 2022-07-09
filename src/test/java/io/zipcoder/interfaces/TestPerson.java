3a2ecac6d07f21dfd1fe2e3b27248615d88b0a8a
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-08 22:27:15 -0400

    person class tests

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,27 @@
 package io.zipcoder.interfaces;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson {
+    @Test
+    public void constructorTest() {
+        long id = 2352;
+        String name = "Johnny";
+        Person individual = new Person(id, name);
+
+        Assert.assertEquals(id, individual.getId());
+        Assert.assertEquals(name, individual.getName());
+    }
+
+    @Test
+    public void setNameTest() {
+        long id = 42;
+        String name = "Ryan";
+        Person individual = new Person(id, name);
+
+        individual.setName("Person");
+
+        Assert.assertEquals("Person", individual.getName());
+    }
 }
