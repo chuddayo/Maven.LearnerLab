3a051df9cf825f1550f49d88cd69f9b6b3b32856
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-08 22:21:03 -0400

    set up person class basics

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    public final long id;
+    String name;
 
+    public Person(long ID, String name) {
+        this.id = ID;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
